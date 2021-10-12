84ab13e276b3ddf1ede7faf7167e7e621b29970b
Zak Warsame <zakwarsamee@gmail.com>
2021-10-12 16:19:04 +0000

    make calculations

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+
+Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: "1900000",mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel:true)
+
+@mens_stores.all.each do |store|
+    puts "#{store[:name]} has a revenue of #{store[:annual_revenue]}"
+ end
+
+ @womens_stores = Store.where("womens_apparel = true")
+
+ @womens_stores.all.each do |store|
+    if store.annual_revenue < 1000000
+     puts "#{store[:name]} has a revenue of #{store[:annual_revenue]}"
+    end
+  end
+
+puts Store.count
